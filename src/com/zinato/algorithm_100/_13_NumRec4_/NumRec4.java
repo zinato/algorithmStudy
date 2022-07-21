b3ca147bb2494b1e6ef93fd298265d015f965be7
Paul <hyunil@braincolla.com>
2022-07-21 17:35:33 +0900

    study : NumRec4 풀이

--- src/com/zinato/algorithm_100/_13_NumRec4_/NumRec4.java
+++ src/com/zinato/algorithm_100/_13_NumRec4_/NumRec4.java
@@ -13,12 +13,37 @@ public class NumRec4 {
    * for, while
    */
   public static void main(String[] args) {
+    //내가 푼 방식
     int n = 4;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+    for (int i = 1; i < n+1; i++) {
+      for (int j = 1; j < n+1; j++) {
+        System.out.printf("%4d", i * j);
+      }
+      System.out.println();
+    }
 
+    System.out.println("===============================");
+
+    //유툽
+    int k = 4;
+    int arr[][] = new int[k][k];
+
+    for (int i = 0; i < k; i++) {
+      for (int j = 0; j < k; j++) {
+        arr[i][j] = (i + 1) * (j + 1);
       }
     }
+
+    for (int i = 0; i < k; i++) {
+      for (int j = 0; j < k; j++) {
+        System.out.printf("%4d", arr[i][j]);
+      }
+      System.out.println();
+    }
+
+
+
+
   }
 
 }
