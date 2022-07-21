3a176cd470b45e4890b9e4fe0a961812745ee74b
Paul <hyunil@braincolla.com>
2022-07-21 16:12:17 +0900

    study: 문제 내용 정리

---
+++ src/com/zinato/algorithm_100/_13_NumRec4_/NumRec4.java
@@ -0,0 +1,24 @@
+package com.zinato.algorithm_100._13_NumRec4_;
+
+public class NumRec4 {
+  /**
+   * 문제 소개
+   * 입력된 수(N) 만큼 N행 N열의 형태로 아래와 같이 출력되는 숫자 사각형을 출력하시오
+   * 4
+   * 1 2 3 4
+   * 2 4 6 8
+   * 3 6 9 12
+   * 4 8 12 16
+   *
+   * for, while
+   */
+  public static void main(String[] args) {
+    int n = 4;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+
+      }
+    }
+  }
+
+}
