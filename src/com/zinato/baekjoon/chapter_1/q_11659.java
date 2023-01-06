56e64ea44511470f70c274756aedd34525d778f6
Paul <hyunil@braincolla.com>
2023-01-06 14:16:51 +0900

    11659 문제 풀이 중

---
+++ src/com/zinato/baekjoon/chapter_1/q_11659.java
@@ -0,0 +1,15 @@
+package com.zinato.baekjoon.chapter_1;
+
+
+import java.util.Arrays;
+
+public class q_11659 {
+  public static void main(String[] args) {
+    int A[] = new int[4];
+    for (int i=1; i <=3; i++) {
+      A[i] = i;
+    }
+    System.out.println(Arrays.toString(A));
+  }
+
+}
