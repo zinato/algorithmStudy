64d1eb45f613ad8dab1086c94e2a131d829cfbca
Paul <hyunil@braincolla.com>
2025-07-28 22:46:37 +0900

    test: 테스트 2

--- src/com/zinato/dev_test/DevTest.java
+++ src/com/zinato/dev_test/DevTest.java
@@ -3,5 +3,7 @@ package com.zinato.dev_test;
 public class DevTest {
     public static void main(String[] args) {
         System.out.println("dev 테스트 입니다.");
+        System.out.println("1");
+        System.out.println("2");
     }
 }
