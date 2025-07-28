52227ff2d9a82f2625c91a171cb4f2ddc5d9651e
Paul <hyunil@braincolla.com>
2025-07-28 23:11:53 +0900

    Revert "test: 테스트 3"
    This reverts commit ae109b23

--- src/com/zinato/dev_test/DevTest.java
+++ src/com/zinato/dev_test/DevTest.java
@@ -5,7 +5,6 @@ public class DevTest {
         System.out.println("dev 테스트 입니다.");
         System.out.println("1");
         System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+        System.out.println("Revert입니다.");
     }
 }
